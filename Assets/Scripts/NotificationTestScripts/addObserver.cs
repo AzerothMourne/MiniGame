5e9f358463308a446e1da1d34294d9a30eae29ec
enqingchen <316215583@qq.com>
2015-04-13 19:59:16 +0800

    MGNotificationCenter&MGFoundtion
    通知中心写好了，大家用起来，示例看NotificationTestScripts里面的脚本。
    
    MGFoundtion暂时封装了Dictionary的一些方法，因为C#的Dictionary实在太难用了。需啊哟使用Dictionary的可以看看

---
+++ Assets/Scripts/NotificationTestScripts/addObserver.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addObserver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public void onClick() {
+        MGNotificationCenter.defaultCenter().addObserver(this, observerSel, "testNotification");
+        MGNotificationCenter.defaultCenter().addObserver(this, observerSel1, "testNotification");
+        MGNotificationCenter.defaultCenter().addObserver(this, observerSel2, "testNotification");
+        MGNotificationCenter.defaultCenter().addObserver(this, observerSel3, "testNotification");
+    }
+    void observerSel(MGNotification notification)
+    {
+        print("收到通知："+notification.name+notification);
+    }
+    void observerSel1(MGNotification notification)
+    {
+        print("收到通知：" + notification.name + notification);
+    }
+    void observerSel2(MGNotification notification)
+    {
+        print("收到通知：" + notification.name + notification);
+    }
+    void observerSel3(MGNotification notification)
+    {
+        print("收到通知：" + notification.name + notification);
+    }
+    public void onClickRemove()
+    {
+        MGNotificationCenter.defaultCenter().removeObserver(this, "testNotification");
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/NotificationTestScripts/postNotification.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class postNotification : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public void onClick()
+    {
+        object objc=new object();
+        MGNotificationCenter.defaultCenter().postNotification("testNotification", objc);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
