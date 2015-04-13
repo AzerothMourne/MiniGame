908cc3d20556a64217a4bd94d94b2b9376958330
enqingchen <316215583@qq.com>
2015-04-13 20:05:40 +0800

    MGGlobalDataCenter
    MGGlobalDataCenter 单例

---
+++ Assets/Scripts/MGFoundation/MGGlobalDataCenter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MGGlobalDataCenter  {
+    private static MGGlobalDataCenter instance;
+    public int rRoleBlood, lRoleBlood;
+
+    private MGGlobalDataCenter()
+    {
+        this.rRoleBlood = 1;
+        this.lRoleBlood = 2;
+	}
+
+    public static MGGlobalDataCenter defaultCenter()
+    {
+		if (instance == null) {
+            instance = new MGGlobalDataCenter();
+		}
+		return instance;
+	}
+}
