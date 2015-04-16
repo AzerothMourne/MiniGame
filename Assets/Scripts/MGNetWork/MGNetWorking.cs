9d2973779bbe9fd756d2aebe9efcd6f3e841ca8b
enqingchen <316215583@qq.com>
2015-04-16 23:00:48 +0800

    修改MGNetWorking相关方法
    改成静态方法方便调用

---
+++ Assets/Scripts/MGNetWork/MGNetWorking.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public class MGNetWorking : MonoBehaviour {
+
+    void Start()
+    {
+        print("Init GlobalData");
+        MGGlobalDataCenter.defaultCenter();
+    }
+	[DllImport("__Internal")]
+	private static extern void _findHost();
+	[DllImport("__Internal")]
+	private static extern void _createHost();
+	[DllImport( "__Internal" )]
+	private static extern void _testUnityToiOS ( string msg);
+	[DllImport( "__Internal" )]
+	private static extern void _sendMessageToPeer ( string msg);
+	
+	public static void findHost()
+	{
+		if(Application.platform==RuntimePlatform.IPhonePlayer)
+			_findHost();
+	}
+	public static void createHost()
+	{
+		if(Application.platform==RuntimePlatform.IPhonePlayer)
+			_createHost();
+	}
+	
+	public static void sendMessageToPeer (string msg){
+		print ("sendMessageToPeer:"+msg);
+		if(Application.platform==RuntimePlatform.IPhonePlayer)
+			_sendMessageToPeer(msg);
+	}
+	public void receiverMessageFromPeer ( string msg)
+	{
+		print ("receiverMessageFromPeer:"+msg+";"+MGGlobalDataCenter.timestamp());
+		MGMsgModel msgModel = JsonMapper.ToObject<MGMsgModel>(msg);
+		MGNotificationCenter.defaultCenter().postNotification(msgModel.eventId,msgModel);
+	}
+}
