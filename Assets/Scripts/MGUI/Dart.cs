b762424dd25f89c9df8b2d3df681b8c3af778937
296023998 <296023998@qq.com>
2015-04-19 21:49:01 +0800

    add  some animation

--- Assets/Scripts/MGUI/Dart.cs
+++ Assets/Scripts/MGUI/Dart.cs
@@ -3,18 +3,43 @@ using System.Collections;
 
 public class Dart : MonoBehaviour {
 
+	private Animator dartAnim; 
+	public bool isPressDartButton;
+	int countDartFrame;
+
 	// Use this for initialization
 	void Start () {
-	
+		isPressDartButton = false;
+		countDartFrame = 0;
+	}
+
+	void Awake() {
+		dartAnim = GameObject.Find("role").GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		//触发放飞镖的动作
+		dartAnim.SetBool ("pressDartButton", isPressDartButton);
+
+		if (isPressDartButton == true) {
+			countDartFrame += 1;
+			print ("countFrame : " + countDartFrame);
+		}
+		if (countDartFrame == 30) {
+			isPressDartButton = false;
+			countDartFrame = 0;
+		//	print ("update ********isPressDartButton : " + isPressDartButton);
+		}
+
 	}
 
 	public void OnMouseDown () {
 		print ("Dart OnMouseDown ");
+		isPressDartButton = true;
+		//print (".....OnMouseDown isPressDartButton : " + isPressDartButton);
+
         if (MGGlobalDataCenter.defaultCenter().isHost == true)
             MGNotificationCenter.defaultCenter().postNotification("useSkillsDart", null);
         else
