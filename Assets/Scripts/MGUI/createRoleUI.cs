5f5a20b23e8c5782d25d39e32e71c26bf2f5ed16
296023998 <296023998@qq.com>
2015-04-20 16:09:53 +0800

    Revert "123"
    This reverts commit 8807034f124855e8c602e4d229ba5522ce34a596.

--- Assets/Scripts/MGUI/createRoleUI.cs
+++ Assets/Scripts/MGUI/createRoleUI.cs
@@ -24,17 +24,17 @@ public class createRoleUI : MonoBehaviour {
         print("createFrontRoleUI");
         GameObject objc= GameObject.Instantiate(dartButton, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, -1)) as GameObject;
         objc.transform.parent = NGUIRoot.transform;
-        objc.transform.localPosition = new Vector3(1130, 520, 0);
+        objc.transform.localPosition = new Vector3(2000, 500, 0);
         objc.transform.localScale = new Vector3(1, 1, 1);
 
         objc = GameObject.Instantiate(roadblockButton, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, -1)) as GameObject;
         objc.transform.parent = NGUIRoot.transform;
-        objc.transform.localPosition = new Vector3(350, -600, 0);
+        objc.transform.localPosition = new Vector3(1000, -950, 0);
         objc.transform.localScale = new Vector3(1, 1, 1);
 
         objc = GameObject.Instantiate(bonesButton, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, -1)) as GameObject;
         objc.transform.parent = NGUIRoot.transform;
-        objc.transform.localPosition = new Vector3(-1000, 400, 0);
+        objc.transform.localPosition = new Vector3(-2000, 500, 0);
         objc.transform.localScale = new Vector3(1, 1, 1);
     }
     public void createLaterRoleUI()
