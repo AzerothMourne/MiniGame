2e2b9eea6a18295ffbf460a2d3ca7614418b3c22
296023998 <296023998@qq.com>
2015-04-24 16:31:15 +0800

    add over UI

---
+++ Assets/Scripts/OverUIScript/overUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class overUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnOverButtonClick() {
+		//isPressOverButton = true;
+		print ("click over again button");
+	}
+}
