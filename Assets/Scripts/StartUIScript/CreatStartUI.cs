06e1b464a07d5f2f6495619b83176baead36b16b
296023998 <296023998@qq.com>
2015-05-01 22:09:29 +0800

    开始界面适配

---
+++ Assets/Scripts/StartUIScript/CreatStartUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatStartUI : MonoBehaviour {
+	public GameObject startObj;
+	public int width, height;
+	public float x_k,y_k;
+
+
+	// Use this for initialization
+	void Start () {
+		x_k = MGGlobalDataCenter.defaultCenter ().pixelWidth / width;
+		y_k = MGGlobalDataCenter.defaultCenter ().pixelHight / height;
+
+		startObj = GameObject.Find("start");
+		startObj.transform.localScale = new Vector3 (x_k,y_k,1);
+		Debug.Log (startObj.transform.localScale);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
