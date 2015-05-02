9a74e77d85a8002390e4456f63aa454172622964
enqingchen <316215583@qq.com>
2015-05-02 17:42:38 +0800

    还是连接问题
    1

--- Assets/MyNetworkTest.cs
+++ Assets/MyNetworkTest.cs
@@ -12,21 +12,19 @@ public class MGConnectModel
     public string ip { get; set; }
     public long timestamp { get; set; }
 }
-public delegate void listenForServerDelegate();
+public delegate string listenForServerDelegate(int port);
 public class MyNetworkTest : MonoBehaviour {
     public int connecttions = 1;
     public UILabel log;
     private Vector3 acceleration;
     public GameObject cube;
-    private listenForServerDelegate udpListen;
-    private bool isCancelListen;
-    private Socket listenSocket;
+	private listenForServerDelegate udpReceive;
+	private bool isCancelListen;
     void Start()
     {
         isCancelListen = false;
-        udpListen = new listenForServerDelegate(listenForServer);
+		udpReceive = new listenForServerDelegate(UDPStartToReceive);
         MGGlobalDataCenter.defaultCenter().serverIp = MGFoundtion.getInternIP();
-        listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);//初始化一个Scoket协议
     }
     public void createHost()
     {
@@ -41,61 +39,51 @@ public class MyNetworkTest : MonoBehaviour {
         if (NetworkPeerType.Disconnected == Network.peerType)
         {
             Debug.Log("find host");
-            InvokeRepeating("tryToConnect", 0.1f, 0.1f);
+            //InvokeRepeating("tryToConnect", 0.1f, 0.1f);
             isCancelListen = false;
-            udpListen.BeginInvoke(new AsyncCallback(callbackMethod), null);
+			udpReceive.BeginInvoke(MGGlobalDataCenter.defaultCenter().mySocketPort, UDPStartToReceiveCallback, null);
         }
     }
     void tryToConnect()
     {
         Debug.Log("tryToConnect");
-        if(isCancelListen == false)
-            MGNetWorking.findHost();
+//        if(isCancelListen == false)
+        MGNetWorking.findHost();
     }
-    void listenForServer()
-    {
-        IPEndPoint receiveIEP = new IPEndPoint(IPAddress.Any, MGGlobalDataCenter.defaultCenter().mySocketPort);//初始化一个侦听局域网内部所有IP和指定端口
-        EndPoint ep = (EndPoint)receiveIEP;
-        listenSocket.Bind(receiveIEP);//绑定这个实例
-        string modelJson = null;
-        while (true)
-        {
-            byte[] buffer = new byte[1024];//设置缓冲数据流
-            listenSocket.ReceiveFrom(buffer, ref ep);//接收数据,并确把数据设置到缓冲流里面
-            modelJson = Encoding.Unicode.GetString(buffer);
-            if (isCancelListen) break;
-            if (modelJson.Length > 0)
-            {
-                MGGlobalDataCenter.defaultCenter().serverIp = modelJson;
-                break;
-            }
-        }
-        listenSocket.Close();
-    }
-    //回调方法
-    static void callbackMethod(IAsyncResult Ias)
-    {
-        listenForServerDelegate Md = (listenForServerDelegate)Ias.AsyncState;
-        Md.EndInvoke(Ias);
-    }
-    public void cancelConnect()
-    {
-        isCancelListen = true;
-        CancelInvoke("UDPSendBroadcast");
-        CancelInvoke("tryToConnect");
-        if (listenSocket != null)
-        {
-            Debug.Log("close socket");
-            try
-            {
-                listenSocket.Shutdown(SocketShutdown.Receive);
-            }
-            catch
-            {
-                Debug.Log("关闭receive失败");
-            }
-            listenSocket.Close();
-        }
+	//线程函数
+	public string UDPStartToReceive(int port)
+	{
+		Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);//初始化一个Scoket协议
+		IPEndPoint iep = new IPEndPoint(IPAddress.Any, MGGlobalDataCenter.defaultCenter().mySocketPort);//初始化一个侦听局域网内部所有IP和指定端口
+		EndPoint ep = (EndPoint)iep;
+		socket.Bind(iep);//绑定这个实例
+		byte[] buffer = new byte[1024];//设置缓冲数据流
+		string ip = null;
+		while (true)
+		{
+			socket.ReceiveFrom(buffer, ref ep);//接收数据,并确把数据设置到缓冲流里面
+			ip = Encoding.Unicode.GetString(buffer);
+			if (ip.Length>0) break;
+		}
+		return ip;
+	}
+	public void UDPStartToReceiveCallback(IAsyncResult data)
+	{
+		//异步执行完成
+		string resultstr = udpReceive.EndInvoke(data);
+		if (resultstr.Length == 0)
+		{
+			udpReceive.BeginInvoke(MGGlobalDataCenter.defaultCenter().mySocketPort, UDPStartToReceiveCallback, null);
+		}
+		else
+		{
+			MGGlobalDataCenter.defaultCenter().serverIp = resultstr;
+			MGNetWorking.findHost();
+		}
+	}
+	public void cancelConnect()
+	{
+
     }
     void Update()
     {
@@ -103,21 +91,21 @@ public class MyNetworkTest : MonoBehaviour {
         {
             if (Network.connections.Length == 1)
             {
-                CancelInvoke();
-                OnConnect();
+				CancelInvoke("UDPSendBroadcast");
+				OnConnect();
             }
                 
         }
         else if (Network.peerType == NetworkPeerType.Client)
         {
-            CancelInvoke();
-            OnConnect();
+			CancelInvoke("tryToConnect");
+			OnConnect();
         }
     }
     public void UDPSendBroadcast()
     {
 		Debug.Log("UDPSendBroadcast"+MGGlobalDataCenter.defaultCenter().serverIp);
-        if (isCancelListen) return;
+//        if (isCancelListen) return;
         Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);//初始化一个Scoket实习,采用UDP传输
         IPEndPoint iep = new IPEndPoint(IPAddress.Broadcast, MGGlobalDataCenter.defaultCenter().mySocketPort);//初始化一个发送广播和指定端口的网络端口实例
         sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);//设置该scoket实例的发送形式
