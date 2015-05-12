4ac6a52c27b662eeb9e70be20129d491af09e239
enqingchen <316215583@qq.com>
2015-05-13 06:14:53 +0800

    播放开场动画
    去手机上测试

---
+++ Assets/Scripts/MGCGScript/MGCGController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class MGCGController: MonoBehaviour
+{
+ 
+	//电影纹理
+	public MovieTexture movTexture;
+    private GameObject uiRoot;
+	void Start()
+	{
+		//设置当前对象的主纹理为电影纹理
+		renderer.material.mainTexture = movTexture;
+		//设置电影纹理播放模式为循环
+		//movTexture.loop = true;
+        if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            movTexture.Play();
+        }
+        else if (Application.platform == RuntimePlatform.IPhonePlayer || Application.platform == RuntimePlatform.Android)
+        {
+            Handheld.PlayFullScreenMovie("kaichangshipin.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+        }
+        try
+        {
+            uiRoot = GameObject.Find("UI Root");
+            uiRoot.SetActive(false);
+        }
+        catch { }
+        
+        
+	}
+    void Update()
+    {
+        if (!movTexture.isPlaying)
+        {
+            try
+            {
+                uiRoot.SetActive(true);
+            }
+            catch { }
+            Debug.Log("Destroy mov");
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/StartUIScript/MGFirstLaunch.cs
+++ Assets/Scripts/StartUIScript/MGFirstLaunch.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class MGFirstLaunch : MonoBehaviour {
-
+    public GameObject cgPlane;
 	// Use this for initialization
 	void Awake () {
         if (MGFoundtion.isFirstLaunch())
@@ -10,6 +10,7 @@ public class MGFirstLaunch : MonoBehaviour {
             Debug.Log("isFirstLaunch=true");
             MGGlobalDataCenter.defaultCenter().isFirstLaunch = true;
             MGFoundtion.setFirstLaunchFlag();
+            GameObject.Instantiate(cgPlane, Vector3.zero, Quaternion.Euler(90, -180, 0));
         }
         MGGlobalDataCenter.defaultCenter().isMingYueGuide = MGFoundtion.isMingYueGuide();
         MGGlobalDataCenter.defaultCenter().isTianYaGuide = MGFoundtion.isTianYaGuide();
