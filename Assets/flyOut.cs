37b7ea90a054394a16a20c9787868be81f82f724
enqingchen <316215583@qq.com>
2015-05-13 04:37:08 +0800

    主界面图片资源替换
    1

--- Assets/flyOut.cs
+++ Assets/flyOut.cs
@@ -13,7 +13,7 @@ public class flyOut : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         y += 0.05f;
-            this.transform.Translate(new Vector3(Random.Range(1f, 2f), Mathf.Sin(y), 0) * Random.Range(2f, 3f) * Time.deltaTime);
+            this.transform.Translate(new Vector3(Random.Range(1f, 3f), Mathf.Sin(y)/5f, 0) * Random.Range(3f, 5f) * Time.deltaTime);
            //this.transform.Translate(new Vector3(Random.Range(1f, 2f), Random.Range(0.1f, 0.2f) * Time.deltaTime));
         
         if (this.transform.position.x > MGFoundtion.pixelToWroldPoint(MGGlobalDataCenter.defaultCenter().pixelWidth, 0).x)
