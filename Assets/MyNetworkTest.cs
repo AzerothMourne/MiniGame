601bc54b5afe6a0404dbcbaf9bf9c634b9d001c0
enqingchen <316215583@qq.com>
2015-05-13 00:05:17 +0800

    解决链接和同步问题
    1

--- Assets/MyNetworkTest.cs
+++ Assets/MyNetworkTest.cs
@@ -7,49 +7,46 @@ using System;
 using System.Net.NetworkInformation;
 using System.Threading;
 using LitJson;
+
 public class MGConnectModel
 {
-    public string ip { get; set; }
-    public long timestamp { get; set; }
+	public string ip { get; set; }
+	public long timestamp { get; set; }
 }
 public delegate string listenForServerDelegate(int port);
 public class MyNetworkTest : MonoBehaviour {
-    public int connecttions = 1;
-    public UILabel log;
-    private Vector3 acceleration;
-    public GameObject cube;
+	public int connecttions = 1;
+	public UILabel log;
+	private Vector3 acceleration;
+	public GameObject cube;
 	private listenForServerDelegate udpReceive;
-	private bool isCancelListen;
-    void Start()
-    {
-        isCancelListen = false;
+	private bool isCancelListen,isReceiveIP;
+	void Start()
+	{
+		isReceiveIP = false;
+		isCancelListen = false;
 		udpReceive = new listenForServerDelegate(UDPStartToReceive);
-        MGGlobalDataCenter.defaultCenter().serverIp = MGFoundtion.getInternIP();
-    }
-    public void createHost()
-    {
-        if (NetworkPeerType.Disconnected == Network.peerType)
-        {
-            MGNetWorking.createHost();
-            InvokeRepeating("UDPSendBroadcast", 0f, 0.1f);
-        }
-    }
-    public void findHost()
-    {
-        if (NetworkPeerType.Disconnected == Network.peerType)
-        {
-            Debug.Log("find host");
-            //InvokeRepeating("tryToConnect", 0.1f, 0.1f);
-            isCancelListen = false;
+		MGGlobalDataCenter.defaultCenter().serverIp = MGFoundtion.getInternIP();
+	}
+	public void createHost()
+	{
+		if (NetworkPeerType.Disconnected == Network.peerType)
+		{
+			MGNetWorking.createHost();
+			InvokeRepeating("UDPSendBroadcast", 0f, 0.1f);
+		}
+	}
+	public void findHost()
+	{
+		if (NetworkPeerType.Disconnected == Network.peerType)
+		{
+			Debug.Log("find host");
+			isCancelListen = false;
+			isReceiveIP = false;
 			udpReceive.BeginInvoke(MGGlobalDataCenter.defaultCenter().mySocketPort, UDPStartToReceiveCallback, null);
-        }
-    }
-    void tryToConnect()
-    {
-        Debug.Log("tryToConnect");
-//        if(isCancelListen == false)
-        MGNetWorking.findHost();
-    }
+		}
+	}
+	
 	//线程函数
 	public string UDPStartToReceive(int port)
 	{
@@ -62,63 +59,85 @@ public class MyNetworkTest : MonoBehaviour {
 		while (true)
 		{
 			socket.ReceiveFrom(buffer, ref ep);//接收数据,并确把数据设置到缓冲流里面
+			if(isCancelListen) break;
 			ip = Encoding.Unicode.GetString(buffer);
-			if (ip.Length>0) break;
+			IPAddress myAddress = null;
+			if (IPAddress.TryParse(ip,out myAddress))
+			{
+				break;
+			}
 		}
-        socket.Close();
+		socket.Close();
 		return ip;
 	}
 	public void UDPStartToReceiveCallback(IAsyncResult data)
 	{
 		//异步执行完成
 		string resultstr = udpReceive.EndInvoke(data);
-		if (resultstr.Length == 0)
+		IPAddress myAddress = null;
+		Debug.Log("resultstr:"+resultstr);
+		if (IPAddress.TryParse(resultstr,out myAddress))
 		{
-			udpReceive.BeginInvoke(MGGlobalDataCenter.defaultCenter().mySocketPort, UDPStartToReceiveCallback, null);
+			MGGlobalDataCenter.defaultCenter().serverIp = resultstr;
+			isReceiveIP=true;
 		}
 		else
 		{
-			MGGlobalDataCenter.defaultCenter().serverIp = resultstr;
-			MGNetWorking.findHost();
+			Debug.Log("ip不合法");
+			if(!isCancelListen)
+				udpReceive.BeginInvoke(MGGlobalDataCenter.defaultCenter().mySocketPort, UDPStartToReceiveCallback, null);
 		}
 	}
 	public void cancelConnect()
 	{
-
-    }
-    void Update()
-    {
-        if (Network.peerType == NetworkPeerType.Server)
-        {
-            if (Network.connections.Length == 1)
-            {
+		isCancelListen = true;
+		UDPSendBroadcast ();
+		CancelInvoke("UDPSendBroadcast");
+	}
+	void Update()
+	{
+		if (isReceiveIP) {
+			Debug.Log("isReceiveIP=true");
+			isReceiveIP = false;
+			NetworkConnectionError connectError = MGNetWorking.findHost();
+			log.text += "\r\nconnect";
+			if(connectError != NetworkConnectionError.NoError && !isCancelListen){
+				log.text += "\r\nconnect faild";
+				udpReceive.BeginInvoke(MGGlobalDataCenter.defaultCenter().mySocketPort, UDPStartToReceiveCallback, null);
+			}
+		}
+		if (Network.peerType == NetworkPeerType.Server)
+		{
+			if (Network.connections.Length == 1)
+			{
 				CancelInvoke("UDPSendBroadcast");
+				MGGlobalDataCenter.defaultCenter().clientIP = Network.connections[0].ipAddress;
+				Debug.Log("clientIP=" + MGGlobalDataCenter.defaultCenter().clientIP);
 				OnConnect();
-            }
-                
-        }
-        else if (Network.peerType == NetworkPeerType.Client)
-        {
-			CancelInvoke("tryToConnect");
+			}
+		}
+		else if (Network.peerType == NetworkPeerType.Client)
+		{
 			OnConnect();
-        }
-    }
-    public void UDPSendBroadcast()
-    {
+		}
+	}
+	public void UDPSendBroadcast()
+	{
 		Debug.Log("UDPSendBroadcast"+MGGlobalDataCenter.defaultCenter().serverIp);
-//        if (isCancelListen) return;
-        Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);//初始化一个Scoket实习,采用UDP传输
-        IPEndPoint iep = new IPEndPoint(IPAddress.Broadcast, MGGlobalDataCenter.defaultCenter().mySocketPort);//初始化一个发送广播和指定端口的网络端口实例
-        sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);//设置该scoket实例的发送形式
-        /*
+		log.text += "\r\n" + MGGlobalDataCenter.defaultCenter().serverIp;
+		//        if (isCancelListen) return;
+		Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);//初始化一个Scoket实习,采用UDP传输
+		IPEndPoint iep = new IPEndPoint(IPAddress.Broadcast, MGGlobalDataCenter.defaultCenter().mySocketPort);//初始化一个发送广播和指定端口的网络端口实例
+		sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);//设置该scoket实例的发送形式
+		/*
         MGConnectModel model = new MGConnectModel();
         model.ip = MGGlobalDataCenter.defaultCenter().serverIp;
         model.timestamp = MGGlobalDataCenter.timestamp();
         */
-        byte[] buffer = Encoding.Unicode.GetBytes(MGGlobalDataCenter.defaultCenter().serverIp);
-        sock.SendTo(buffer, iep);
-        sock.Close();
-    }
+		byte[] buffer = Encoding.Unicode.GetBytes(MGGlobalDataCenter.defaultCenter().serverIp);
+		sock.SendTo(buffer, iep);
+		sock.Close();
+	}
 	void OnConnect()
 	{
 		/*
@@ -127,9 +146,15 @@ public class MyNetworkTest : MonoBehaviour {
             Network.Instantiate(cube, transform.position, transform.rotation, 0);
             cubeInitialed = true;
         }*/
-        //连接成功 需要切换场景
-        //MGNotificationCenter.defaultCenter().postNotification(LoadSenceEnum.LoadLevel, "gameScene1");
-        MGGlobalDataCenter.defaultCenter().isSingle = false;
-        Application.LoadLevel("gameScene1");
-    }
+		//连接成功 需要切换场景
+		//MGNotificationCenter.defaultCenter().postNotification(LoadSenceEnum.LoadLevel, "gameScene1");
+		MGGlobalDataCenter.defaultCenter().isSingle = false;
+		Application.LoadLevel("gameScene1");
+	}
+	void OnApplicationQuit(){
+		Debug.Log ("OnApplicationQuit");
+		isCancelListen = true;
+		UDPSendBroadcast ();
+		CancelInvoke("UDPSendBroadcast");
+	}
 }
--- Assets/Scripts/MGFoundation/MGGlobalDataCenter.cs
+++ Assets/Scripts/MGFoundation/MGGlobalDataCenter.cs
@@ -6,106 +6,109 @@ using System;
 
 public class MGMsgModel{
 	public string eventId { get; set;}
-    public string gameobjectName { get; set; }
+	public string gameobjectName { get; set; }
 	public long timestamp { get; set; }
-    public string posJson;
-    public string tag;
-    public string name;
+	public string posJson;
+	public string tag;
+	public string name;
 }
 
 
 public class MGGlobalDataCenter  {
-    public float NGUI_ButtonWidth;
-    public bool isNetworkViewEnable;
-    public bool isFrontRoler,isLaterRoler,isBigSkilling,isStop,isSingle;
-    public int connecttions;
-    public int listenPort,mySocketPort,UPNPPort,SyncPort;
-    private static MGGlobalDataCenter instance;
-    public int rRoleBlood, lRoleBlood;
-    public GameObject role, roleLater;
-    private string _serverIp;
+	public float NGUI_ButtonWidth;
+	public bool isNetworkViewEnable;
+	public bool isFrontRoler,isLaterRoler,isBigSkilling,isStop,isSingle;
+	public int connecttions;
+	public int listenPort,mySocketPort,UPNPPort,SyncPort;
+	private static MGGlobalDataCenter instance;
+	public int rRoleBlood, lRoleBlood;
+	public GameObject role, roleLater;
+	private string _serverIp;
 	public float UIScale,roadOrignY;
-    public float totalGameTime;
-    public string overSenceUIName;
-    public int dartIndex;
-    public bool isFirstLaunch,isMingYueGuide,isTianYaGuide,isGameOver;
+	public float totalGameTime;
+	public string overSenceUIName,clientIP;
+	public int dartIndex;
+	public bool isFirstLaunch,isMingYueGuide,isTianYaGuide,isGameOver;
 	//@aragornwang
 	//play music 
 	public bool isDartHit;
 	public bool isDartDefence;
 	public bool isDartRelease;
 	public bool isKillMingyue;
+	public bool isPlayKill;
 	public bool isRoadBlockHit;
 	public bool isRoadBlockDefence;
-
+	
 	public bool isVictory;
 	public bool isDefeat;
 	public bool isFlash;
 	public bool isSprint;
 	public bool isRun;
-
-    public string serverIp
-    {
-        get
-        {
-            return _serverIp;
-        }
-        set
-        {
-            _serverIp = value;
-            isNetworkViewEnable = true;
-        }
-    }
-    public Vector3 leftBottomPos, rightTopPos,roleFrontPos,roleLaterPos;
-    /// <summary>
-    /// 屏幕上下左右边框的坐标值(世界坐标)
-    /// </summary>
-    public float screenLiftX, screenRightX,screenTopY,screenBottomY;
-    /// <summary>
-    /// 屏幕的像素宽高
-    /// </summary>
-    public float pixelWidth, pixelHight;
-    private MGGlobalDataCenter()
-    {
-        Debug.Log("Init GlobalData");
-        this.isFrontRoler = true;
-        this.isLaterRoler = !this.isFrontRoler;
-        this.overSenceUIName = null;
-        backToDefaultValues();
+	
+	public string serverIp
+	{
+		get
+		{
+			return _serverIp;
+		}
+		set
+		{
+			_serverIp = value;
+			isNetworkViewEnable = true;
+		}
 	}
-    public void backToDefaultValues()
-    {
-        this.isGameOver = false;
-        this.isMingYueGuide = false;
-        this.isTianYaGuide = false;
-        this.isFirstLaunch = false;
-        this.isSingle = true;
-        this.dartIndex = 0;
-        Time.timeScale = 1;
-        this.totalGameTime = 60f;
-        this.isStop = false;
-        this.isBigSkilling = false;
-        this.role = null;
-        this.roleLater = null;
-        this.connecttions = 1;
-        this.listenPort = 8899;
-        this.SyncPort = 12000;
-        this.mySocketPort = 10000;
-        this.serverIp = "127.0.0.0";
-        this.isNetworkViewEnable = false;
-        this.rRoleBlood = 1;
-        this.lRoleBlood = 2;
-        this.screenLiftX = -8.9f;
-        this.screenRightX = -1 * this.screenLiftX;
-
-        this.screenTopY = 5f;
-        this.screenBottomY = -1 * this.screenTopY;
-        this.NGUI_ButtonWidth = 1.65f;
-        this.UIScale = 2f;
-        this.roadOrignY = -1000;
-        this.leftBottomPos = this.rightTopPos = this.roleFrontPos = this.roleLaterPos = Vector3.zero;
-        this.isDartHit = false;
-        this.isDartDefence = false;
+	public Vector3 leftBottomPos, rightTopPos,roleFrontPos,roleLaterPos;
+	/// <summary>
+	/// 屏幕上下左右边框的坐标值(世界坐标)
+	/// </summary>
+	public float screenLiftX, screenRightX,screenTopY,screenBottomY;
+	/// <summary>
+	/// 屏幕的像素宽高
+	/// </summary>
+	public float pixelWidth, pixelHight;
+	private MGGlobalDataCenter()
+	{
+		Debug.Log("Init GlobalData");
+		this.isFrontRoler = false;
+		this.isLaterRoler = !this.isFrontRoler;
+		this.overSenceUIName = null;
+		backToDefaultValues();
+	}
+	public void backToDefaultValues()
+	{
+		this.clientIP = null;
+		this.isPlayKill = false;
+		this.isGameOver = false;
+		this.isMingYueGuide = false;
+		this.isTianYaGuide = false;
+		this.isFirstLaunch = false;
+		this.isSingle = true;
+		this.dartIndex = 0;
+		Time.timeScale = 1;
+		this.totalGameTime = 60f;
+		this.isStop = false;
+		this.isBigSkilling = false;
+		this.role = null;
+		this.roleLater = null;
+		this.connecttions = 1;
+		this.listenPort = 8899;
+		this.SyncPort = 12000;
+		this.mySocketPort = 15438;
+		this.serverIp = "127.0.0.0";
+		this.isNetworkViewEnable = false;
+		this.rRoleBlood = 1;
+		this.lRoleBlood = 2;
+		this.screenLiftX = -8.9f;
+		this.screenRightX = -1 * this.screenLiftX;
+		
+		this.screenTopY = 5f;
+		this.screenBottomY = -1 * this.screenTopY;
+		this.NGUI_ButtonWidth = 1.65f;
+		this.UIScale = 2f;
+		this.roadOrignY = -1000;
+		this.leftBottomPos = this.rightTopPos = this.roleFrontPos = this.roleLaterPos = Vector3.zero;
+		this.isDartHit = false;
+		this.isDartDefence = false;
 		this.isRoadBlockHit = false;
 		this.isVictory = false;
 		this.isDefeat = false;
@@ -113,19 +116,19 @@ public class MGGlobalDataCenter  {
 		this.isFlash = false;
 		this.isSprint = false;
 		this.isRun = false;
-    }
-    public static MGGlobalDataCenter defaultCenter()
-    {
+	}
+	public static MGGlobalDataCenter defaultCenter()
+	{
 		if (instance == null) {
-            instance = new MGGlobalDataCenter();
+			instance = new MGGlobalDataCenter();
 		}
 		return instance;
 	}
-    public static long timestamp()
-    {
-        TimeSpan ts = DateTime.Now - DateTime.Parse("1970-1-1");
-        return (long)Math.Floor(ts.TotalMilliseconds);
-    }
-    
-
+	public static long timestamp()
+	{
+		TimeSpan ts = DateTime.Now - DateTime.Parse("1970-1-1");
+		return (long)Math.Floor(ts.TotalMilliseconds);
+	}
+	
+	
 }
