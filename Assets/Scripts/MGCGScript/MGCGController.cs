2e8ed4c4a3e410bd28e03fcb0475f070627fe48b
enqingchen <316215583@qq.com>
2015-05-13 10:54:30 +0800

    开始场景UI位置调整
    1

--- Assets/Scripts/MGCGScript/MGCGController.cs
+++ Assets/Scripts/MGCGScript/MGCGController.cs
@@ -1,49 +1,67 @@
-﻿using UnityEngine;
+﻿
+//#define PCORMOBILE
+using UnityEngine;
 using System.Collections;
 
 public class MGCGController: MonoBehaviour
 {
 	
 	//电影纹理
-	//	public MovieTexture movTexture;
-	private GameObject uiRoot,Audio,StartScript,CreatStartUI,BirdController;
+#if PCORMOBILE
+	public MovieTexture movTexture;
+#endif
+    private GameObject uiRoot,Audio,StartScript,CreatStartUI,BirdController;
 	private bool isEnd;
 	void Start()
 	{
 		isEnd = false;
-		//		//设置当前对象的主纹理为电影纹理
-		//		renderer.material.mainTexture = movTexture;
-		//		//设置电影纹理播放模式为循环
-		//		//movTexture.loop = true;
-		//		if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.OSXEditor)
-		//        {
-		//            movTexture.Play();
-		//        }
-		//        else if (Application.platform == RuntimePlatform.IPhonePlayer || Application.platform == RuntimePlatform.Android)
-		//        {
-		//            Handheld.PlayFullScreenMovie("kaichangshipin.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
-		//        }
-		try
-		{
-			uiRoot = GameObject.Find("UI Root");
-			Audio = GameObject.Find("Audio");
-			StartScript= GameObject.Find("StartScript");
-			CreatStartUI=GameObject.Find("CreatStartUI");
-			BirdController=GameObject.Find("BirdController");
-			uiRoot.SetActive(false);
-			Audio.SetActive(false);
-			StartScript.SetActive(false);
-			CreatStartUI.SetActive(false);
-			BirdController.SetActive(false);
-		}
-		catch { }
+        try
+        {
+            uiRoot = GameObject.Find("UI Root");
+            Audio = GameObject.Find("Audio");
+            StartScript = GameObject.Find("StartScript");
+            CreatStartUI = GameObject.Find("CreatStartUI");
+            BirdController = GameObject.Find("BirdController");
+            uiRoot.SetActive(false);
+            Audio.SetActive(false);
+            StartScript.SetActive(false);
+            CreatStartUI.SetActive(false);
+            BirdController.SetActive(false);
+        }
+        catch { }
+#if PCORMOBILE
+        //设置当前对象的主纹理为电影纹理
+        renderer.material.mainTexture = movTexture;
+        //设置电影纹理播放模式为循环
+        //movTexture.loop = true;
+        movTexture.Play();
+#else
+        
 		Handheld.PlayFullScreenMovie("kaichangshipin.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
 
+#endif
 		
 		
 	}
 	void Update()
 	{
+#if PCORMOBILE
+        if (!movTexture.isPlaying)
+        {
+            try
+            {
+                uiRoot.SetActive(true);
+                Audio.SetActive(true);
+                StartScript.SetActive(true);
+                CreatStartUI.SetActive(true);
+                BirdController.SetActive(true);
+            }
+            catch { }
+            Debug.Log("Destroy mov");
+            Destroy(this.gameObject);
+        }
+#else
+        
 		if(!isEnd){
 			isEnd=true;
 			try
@@ -58,16 +76,9 @@ public class MGCGController: MonoBehaviour
 			Debug.Log("Destroy mov");
 			Destroy(this.gameObject);
 		}
-		//        if (!movTexture.isPlaying)
-		//        {
-		//            try
-		//            {
-		//                uiRoot.SetActive(true);
-		//				Audio.SetActive(true);
-		//            }
-		//            catch { }
-		//            Debug.Log("Destroy mov");
-		//            Destroy(this.gameObject);
-		//        }
+
+#endif
+        
 	}
+    
 }
