dab01be383eabf4316a27f9a92c3350bf3388d30
enqingchen <316215583@qq.com>
2015-05-13 08:02:13 +0800

    开场动画iOS调通
    1

--- Assets/Scripts/MGCGScript/MGCGController.cs
+++ Assets/Scripts/MGCGScript/MGCGController.cs
@@ -1,46 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
- 
+
 public class MGCGController: MonoBehaviour
 {
- 
+	
 	//电影纹理
-	public MovieTexture movTexture;
-    private GameObject uiRoot;
+	//	public MovieTexture movTexture;
+	private GameObject uiRoot,Audio,StartScript,CreatStartUI,BirdController;
+	private bool isEnd;
 	void Start()
 	{
-		//设置当前对象的主纹理为电影纹理
-		renderer.material.mainTexture = movTexture;
-		//设置电影纹理播放模式为循环
-		//movTexture.loop = true;
-        if (Application.platform == RuntimePlatform.WindowsEditor)
-        {
-            movTexture.Play();
-        }
-        else if (Application.platform == RuntimePlatform.IPhonePlayer || Application.platform == RuntimePlatform.Android)
-        {
-            Handheld.PlayFullScreenMovie("kaichangshipin.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
-        }
-        try
-        {
-            uiRoot = GameObject.Find("UI Root");
-            uiRoot.SetActive(false);
-        }
-        catch { }
-        
-        
+		isEnd = false;
+		//		//设置当前对象的主纹理为电影纹理
+		//		renderer.material.mainTexture = movTexture;
+		//		//设置电影纹理播放模式为循环
+		//		//movTexture.loop = true;
+		//		if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.OSXEditor)
+		//        {
+		//            movTexture.Play();
+		//        }
+		//        else if (Application.platform == RuntimePlatform.IPhonePlayer || Application.platform == RuntimePlatform.Android)
+		//        {
+		//            Handheld.PlayFullScreenMovie("kaichangshipin.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+		//        }
+		try
+		{
+			uiRoot = GameObject.Find("UI Root");
+			Audio = GameObject.Find("Audio");
+			StartScript= GameObject.Find("StartScript");
+			CreatStartUI=GameObject.Find("CreatStartUI");
+			BirdController=GameObject.Find("BirdController");
+			uiRoot.SetActive(false);
+			Audio.SetActive(false);
+			StartScript.SetActive(false);
+			CreatStartUI.SetActive(false);
+			BirdController.SetActive(false);
+		}
+		catch { }
+		Handheld.PlayFullScreenMovie("kaichangshipin.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+
+		
+		
 	}
-    void Update()
-    {
-        if (!movTexture.isPlaying)
-        {
-            try
-            {
-                uiRoot.SetActive(true);
-            }
-            catch { }
-            Debug.Log("Destroy mov");
-            Destroy(this.gameObject);
-        }
-    }
+	void Update()
+	{
+		if(!isEnd){
+			isEnd=true;
+			try
+			{
+				uiRoot.SetActive(true);
+				Audio.SetActive(true);
+				StartScript.SetActive(true);
+				CreatStartUI.SetActive(true);
+				BirdController.SetActive(true);
+			}
+			catch { }
+			Debug.Log("Destroy mov");
+			Destroy(this.gameObject);
+		}
+		//        if (!movTexture.isPlaying)
+		//        {
+		//            try
+		//            {
+		//                uiRoot.SetActive(true);
+		//				Audio.SetActive(true);
+		//            }
+		//            catch { }
+		//            Debug.Log("Destroy mov");
+		//            Destroy(this.gameObject);
+		//        }
+	}
 }
